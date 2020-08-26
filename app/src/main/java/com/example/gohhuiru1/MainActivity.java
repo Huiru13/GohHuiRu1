d0e41abc254507f6fe6048051357afca92b12ec2
Lein Yi Shun <eason7716@gmail.com>
2020-08-26 15:08:33 +0800

    commit by Lein Yi Shun

--- app/src/main/java/com/example/gohhuiru1/MainActivity.java
+++ app/src/main/java/com/example/gohhuiru1/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.gohhuiru1;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.content.ContextCompat;
 
@@ -14,26 +15,24 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
 import java.util.ArrayList;
 import java.util.List;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-    ViewGroup layout;
-
-    String seats = "_UUUAAARRRR_/"
-            + "_________________/"
-            + "UU__AARRR__RR/"
-            + "UU__UAAAA__AA/"
-            + "AA__AAAAA__AA/"
-            + "AA__AAURR__AA/"
-            + "UU__UURRR__AA/"
-            + "AA__AARAA__UU/"
-            + "AA__AAUUU__RR/"
-            + "AA__UUURR__RR/"
-            + "_________________/"
-            + "UU_AAAUUUU_RR/"
-            + "RR_AAAAAAA_AA/"
-            + "_________________/";
+    ViewGroup layoutGroup;
+    FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+    DatabaseReference m1;
+    //String seats = "_UUUAAARRRR_/_________________/UU__AARRR__RR/UU__UAAAA__AA/AA__AAAAA__AA/AA__AAURR__AA/UU__UURRR__AA/AA__AARAA__UU/AA__AAUUU__RR/AA__UUURR__RR/_________________/UU_AAAUUUU_RR/RR_AAAAAAA_AA/_________________/";
+    // new
+    String seats;
+    LinearLayout layoutSeat;
+    LinearLayout layout;
 
     List<TextView> seatViewList = new ArrayList<>();
     int seatSize = 70;
@@ -50,22 +49,22 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        m1 = firebaseDatabase.getReference("movie").child("m1");
+        layoutGroup = findViewById(R.id.layoutSeat);
 
-        layout = findViewById(R.id.layoutSeat);
+        //seats = "/" + seats;
 
-        seats = "/" + seats;
-
-        LinearLayout layoutSeat = new LinearLayout(this);
+        layoutSeat = new LinearLayout(this);
         LinearLayout.LayoutParams params = new LinearLayout.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT);
         layoutSeat.setOrientation(LinearLayout.VERTICAL);
         layoutSeat.setLayoutParams(params);
         layoutSeat.setPadding(8 * seatGaping, 8 * seatGaping, 8 * seatGaping, 8 * seatGaping);
-        layout.addView(layoutSeat);
+        layoutGroup.addView(layoutSeat);
 
-        LinearLayout layout = null;
+        layout = null;
 
 
-
+/*
         for (int index = 0; index < seats.length(); index++) {
 
             if (seats.charAt(index) == '/') {
@@ -137,6 +136,93 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 layout.addView(view);
             }
         }
+
+ */
+        // take firebase
+        m1.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                seats = dataSnapshot.child("movie_seat").getValue(String.class);
+                seats = "/" + seats;
+                for (int index = 0; index < seats.length(); index++) {
+
+                    if (seats.charAt(index) == '/') {
+                        layout = new LinearLayout(MainActivity.this);
+                        layout.setOrientation(LinearLayout.HORIZONTAL);
+                        layoutSeat.addView(layout);
+                    } else if (seats.charAt(index) == 'U') {
+                        count++;
+                        TextView view = new TextView(MainActivity.this);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                        layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                        view.setLayoutParams(layoutParams);
+                        view.setPadding(0, 0, 0, 2 * seatGaping);
+                        view.setId(count);
+                        view.setGravity(Gravity.CENTER);
+                        view.setBackgroundResource(R.drawable.ic_seats_booked);
+                        view.setTextColor(Color.WHITE);
+                        view.setBackgroundColor(Color.RED);
+                        view.setTag(STATUS_BOOKED);
+                        view.setText(count + "");
+                        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                        layout.addView(view);
+                        seatViewList.add(view);
+                        view.setOnClickListener(MainActivity.this);
+                    } else if (seats.charAt(index) == 'R') {
+                        count++;
+                        TextView view = new TextView(MainActivity.this);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                        layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                        view.setLayoutParams(layoutParams);
+                        view.setPadding(0, 0, 0, 2 * seatGaping);
+                        view.setId(count);
+                        view.setGravity(Gravity.CENTER);
+                        view.setBackgroundResource(R.drawable.ic_seats_reserved);
+                        view.setBackgroundColor(Color.GRAY);
+                        view.setText(count + "");
+                        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                        view.setTextColor(Color.BLACK);
+                        view.setTag(STATUS_RESERVED);
+                        layout.addView(view);
+                        seatViewList.add(view);
+                        view.setOnClickListener(MainActivity.this);
+                    } else if (seats.charAt(index) == 'A') {
+                        count++;
+                        TextView view = new TextView(MainActivity.this);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                        layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                        view.setLayoutParams(layoutParams);
+                        view.setPadding(0, 0, 0, 2 * seatGaping);
+                        view.setId(count);
+                        view.setGravity(Gravity.CENTER);
+                        view.setBackgroundResource(R.drawable.ic_seats_book);
+                        view.setBackgroundColor(Color.BLUE);
+                        view.setText(count + "");
+                        view.setTextSize(TypedValue.COMPLEX_UNIT_DIP, 9);
+                        view.setTextColor(Color.WHITE);
+                        view.setTag(STATUS_AVAILABLE);
+                        layout.addView(view);
+                        seatViewList.add(view);
+                        view.setOnClickListener(MainActivity.this);
+                        seats = seats.substring(0, index) + 'U' + seats.substring(index + 1);
+                    } else if (seats.charAt(index) == '_') {
+                        TextView view = new TextView(MainActivity.this);
+                        LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(seatSize, seatSize);
+                        layoutParams.setMargins(seatGaping, seatGaping, seatGaping, seatGaping);
+                        view.setLayoutParams(layoutParams);
+                        view.setBackgroundColor(Color.TRANSPARENT);
+                        view.setText("");
+                        layout.addView(view);
+                    }
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
     }
 
     @Override
